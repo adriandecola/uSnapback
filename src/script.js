d9ae3c3ff26a8ad406f3eb0985c8fb1408bb1e51
Adrian deCola <adriandecola@gmail.com>
2025-04-08 11:49:45 -0400

    Add top level file description comments

---
+++ src/script.js
@@ -0,0 +1,6 @@
+/*
+File:           script.js
+Description:    Main JavaScript file for interactivity, functionality, and processing.
+Author:         Adrian deCola
+Relative Path:  uSnapback/src/script.js
+*/
